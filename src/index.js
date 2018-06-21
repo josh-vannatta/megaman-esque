64175368378f24a24849fe409b9f10ca01bc3a27
Josh Van Natta <jjvannatta88@gmail.com>
2018-06-21 15:27:15 -0600

    Application frame added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
+// require('bootstrap');
 
 function component() {
   let element = document.createElement('canvas');
-  element.className = 'foo';
+  element.className = 'w-100';
   element.id = 'main';
-  element.innerHTML = _.join(['howdy', 'patna!']);
+  // element.innerHTML = _.join(['howdy', 'patna!']);
 
   return element;
 }
